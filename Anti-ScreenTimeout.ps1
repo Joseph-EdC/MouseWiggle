b4bd5d1be45444db7900c2e1c8f961f6b9a0cc35
Joseph-EdC <107596486+Joseph-EdC@users.noreply.github.com>
2024-09-17 13:31:42 -0500

    Add files via upload

---
+++ Anti-ScreenTimeout.ps1
@@ -0,0 +1,23 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+
+Write-Output "Central Utility Plant Mouse Wiggle"
+Write-Output "Press Ctrl+Shift to Exit"
+
+while ($true) {
+    
+    $currentPos = [System.Windows.Forms.Cursor]::Position
+
+    $x = $currentPos.X + 10
+    $y = $currentPos.Y + 10
+
+    
+    [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x, $y)
+
+    Start-Sleep -Seconds 2
+
+    [System.Windows.Forms.Cursor]::Position = $currentPos
+
+    Start-Sleep -Seconds 60
+
+}
+
